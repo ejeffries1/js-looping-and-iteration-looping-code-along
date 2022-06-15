6d7be5d2ddb46fd869d9caf798d706a16747dbf1
evan <ejeffries14@gmail.com>
2022-06-15 15:04:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(cards, event) {
+    let thankYou = []
+    for (let i = 0; i < cards.length; i++) {
+        thankYou.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+      }
+      return thankYou
+    }
+
+function countDown(countdown) {
+    while (countdown > 0) {
+    console.log(countdown);
+    countdown -= 1;
+    }
+    console.log(countdown)
+}
+
